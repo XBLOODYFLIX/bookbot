590583cafc6b4b4fed5943c9c6c2ed3f3cd6f50f
XBLOODYFLIX <felix.geissinger@gmail.com>
2025-12-04 09:06:18 +0100

    project done

--- main.py
+++ main.py
@@ -21,10 +21,7 @@ def main(book_path):
         num = entry["num"]
         if char.isalpha() == True:
             print(f"{char}: {num}")
-    
-    #char = sorted_list
-    
-
+         
     print("============= END ===============")
     
 def get_book_text(book_path):
@@ -39,9 +36,7 @@ if len(sys.argv) != 2:
     sys.exit(1)
 
 book_path = sys.argv[1]
-
 main(book_path)
 
 
 print(f"debug: sys.argv -- sys.argv[0] = {sys.argv[0]} -- sys.argv[1] = {sys.argv[1]}")
-#main(sys.argv[1])
